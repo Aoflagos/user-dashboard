0e5ce3c13df3a1f77cdfd32ef58de96c1e50d6ad
Bolaji Are <54356190+Aoflagos@users.noreply.github.com>
2022-02-21 09:59:04 +0000

    Add files via upload

--- User-dashboard/main.js
+++ User-dashboard/main.js
@@ -2,3 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const sidenav = document.querySelectorAll(".sidenav");
     const sidenavInstance = M.Sidenav.init(sidenav);
   });
+
+  /**inputs */
+  
+  $(document).ready(function() {
+    M.updateTextFields();
+  });
+
+
+  /**profile tabs */
+
+   
+
+  $(document).ready(function(){
+    $('.tabs').tabs();
+  });
